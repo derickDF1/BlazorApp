930ad882910cb3a6605d76bc895fa6be3e02bb10
derickcux2023 <derickcux2023@LAB4-21>
2024-04-22 09:41:59 -0600

    clase 22-04-2024

---
+++ Reporte.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp
+{
+    public class Reporte
+    {
+        public string nombreAlumno { get; set; }
+        public string nombreTaller { get; set; }
+    }
+}
